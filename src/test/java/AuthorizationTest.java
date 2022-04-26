44b14fbc34fd9ccdd0fc53f3bd49303057e3eb98
Ilnur Mameshev <imameshev@inbox.ru>
2022-04-26 20:19:35 +0300

    added positive test

--- src/test/java/AuthorizationTest.java
+++ src/test/java/AuthorizationTest.java
@@ -1,2 +1,25 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static manager.DataGenerator.Registration.registerUser;
+
 public class AuthorizationTest {
+    @BeforeEach
+    public void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    public void shouldCheckRegisteredActiveUser() {
+        var registeredUser = registerUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $(".button").shouldHave(Condition.text("Продолжить")).click();
+        $(".heading").shouldBe(Condition.text("Личный кабинет"));
+    }
 }
