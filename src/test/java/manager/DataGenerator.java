9e9dd4ade05097d65914d2e8704abd7890bae562
Ilnur Mameshev <imameshev@inbox.ru>
2022-04-26 15:04:53 +0300

    added statics methods

--- src/test/java/manager/DataGenerator.java
+++ src/test/java/manager/DataGenerator.java
@@ -1,10 +1,13 @@
 package manager;
 
+import com.github.javafaker.Faker;
 import domain.UserData;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,6 +19,12 @@ public class DataGenerator {
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
             .build();
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+
+    }
 
     private static void sendRequest(UserData user) {
         //Запрос
@@ -30,4 +39,29 @@ public class DataGenerator {
                 .then()                         //"тогда ожидаем"
                 .statusCode(200);               //Код 200, все хорошо
     }
+
+    public static String generateLogin() {
+        return faker.name().firstName();
+    }
+
+    public static String generatePassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+
+        }
+
+        public static UserData generateUser(String status) {
+            return new UserData(generateLogin(), generatePassword(), status);
+        }
+
+        public static UserData registerUser(String status) {
+            UserData registration = generateUser(status);
+            sendRequest(registration);
+            return registration;
+        }
+    }
+
 }
